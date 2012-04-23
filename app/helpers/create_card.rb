35097a8a2776cdbd7e73567f8f1496da26973b8c
Jeffrey Basurto <jeffreybasurto@gmail.com>
2012-04-23 22:50:38 +0800

    added a lot of styling for cards

--- app/helpers/create_card.rb
+++ app/helpers/create_card.rb
@@ -1,5 +1,5 @@
 RankMyThings.helpers do
   def create_card(content, extra_classes)
-    "<li class='ui-state-default card #{extra_classes}'>#{content}</li>"
+    "<li class='ui-state-default card #{extra_classes}'><table><td style='height: 100px;vertical-align:middle'>#{content}</td></table></li>"
   end
 end
