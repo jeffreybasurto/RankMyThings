20e72ec8cdec23fc3e1ede677b6e4e248e9dbb48
Jeffrey Basurto <jeffreybasurto@gmail.com>
2012-04-23 19:02:16 +0800

    created project

---
+++ config/database.rb
@@ -0,0 +1,19 @@
+##
+# A MySQL connection:
+# DataMapper.setup(:default, 'mysql://user:password@localhost/the_database_name')
+#
+# # A Postgres connection:
+# DataMapper.setup(:default, 'postgres://user:password@localhost/the_database_name')
+#
+# # A Sqlite3 connection
+# DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "development.db"))
+#
+
+DataMapper.logger = logger
+DataMapper::Property::String.length(255)
+
+case Padrino.env
+  when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "rank_my_things_development.db"))
+  when :production  then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "rank_my_things_production.db"))
+  when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "rank_my_things_test.db"))
+end
