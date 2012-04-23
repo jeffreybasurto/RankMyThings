a9e83df1d0d9ff8e3551b422b65662954a392d38
Jeffrey Basurto <jeffreybasurto@gmail.com>
2012-04-23 19:32:02 +0800

    changed production to use correct postgres database path from environment variable

--- config/database.rb
+++ config/database.rb
@@ -13,7 +13,7 @@ DataMapper.logger = logger
 DataMapper::Property::String.length(255)
 
 case Padrino.env
+  when :production then DataMapper.setup(:default, ENV["DATABASE_URL"])
   when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "rank_my_things_development.db"))
-  when :production  then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "rank_my_things_production.db"))
   when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "rank_my_things_test.db"))
 end
